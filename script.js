6f65e404cca119db7b88add6f7fafc126d311430
RomainChardon <chardon.romain26@gmail.com>
2024-03-02 23:04:48 +0100

    first

---
+++ script.js
@@ -0,0 +1,10 @@
+// document.querySelectorAll('.link > img').forEach(function(element) {
+//     console.log(element);
+//     element.style.maxHeight = getRandomInt(200, 400) + 'px';
+// });
+//
+// function getRandomInt(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
